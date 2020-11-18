823d4616235ce352d14504aa3c97ff82104eb833
Elaine Wu <63991329+Elaine-Wu99@users.noreply.github.com>
2020-11-18 09:55:59 -0600

    Add files via upload

---
+++ PrintArray.java
@@ -0,0 +1,10 @@
+public class PrintArray{
+   // int[] arr;
+    public void printArray(int[] arr){
+        int len = arr.length;
+        for(int i=0;i<len;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+}
